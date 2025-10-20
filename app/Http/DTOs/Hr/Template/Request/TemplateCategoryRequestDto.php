75437158cba70f653a2b0650bc3c8ada747c9900
ildirim.huseynov <ildirim.huseynov@push30.az>
2025-10-20 17:01:15 +0400

    fix

--- app/Http/DTOs/Hr/Template/Request/TemplateCategoryRequestDto.php
+++ app/Http/DTOs/Hr/Template/Request/TemplateCategoryRequestDto.php
@@ -15,7 +15,7 @@ class TemplateCategoryRequestDto extends CoreData
         #[DataCollectionOf(QuestionAndOrderRequestDto::class)]
         public DataCollection $questions,
         #[Exists('question_categories', 'id')]
-        public null|int $questionCategoryId,
+        public Optional|null|int $questionCategoryId = null,
         #[DigitsBetween(1, 10000)]
         public int|Optional $duration = 0,
         public int|Optional $orderNumber = 0,
