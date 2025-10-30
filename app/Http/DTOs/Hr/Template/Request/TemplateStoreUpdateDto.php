a671750d668b5403ae02f40c4bfc8b66f2cff9d1
ildirim.huseynov <ildirim.huseynov@push30.az>
2025-10-30 21:04:42 +0400

    fix

---
+++ app/Http/DTOs/Hr/Template/Request/TemplateStoreUpdateDto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\DTOs\Hr\Template\Request;
+
+use App\Http\DTOs\CoreData;
+use Spatie\LaravelData\Attributes\Validation\Between;
+use Spatie\LaravelData\Attributes\Validation\Exists;
+use Spatie\LaravelData\Attributes\Validation\Max;
+use Spatie\LaravelData\Attributes\Validation\Min;
+
+class TemplateStoreUpdateDto extends CoreData
+{
+    public function __construct(
+        #[Between(1, 11)]
+        #[Exists('job_subcategories', 'id')]
+        public int $jobSubcategoryId,
+
+        #[Between(1, 11)]
+        #[Exists('template_types', 'id')]
+        public int $templateTypeId,
+
+        #[Exists('languages', 'id')]
+        public int $languageId,
+
+        #[Min(3)]
+        #[Max(100)]
+        public string $name,
+    ) {
+    }
+}
