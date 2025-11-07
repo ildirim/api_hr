fa8e22f67f8c88b1b344f9d6b56f56c0e5a64661
ildirim.huseynov <ildirim.huseynov@push30.az>
2025-11-07 15:07:36 +0400

    fix

--- app/Http/Repositories/Hr/TemplateRepository.php
+++ app/Http/Repositories/Hr/TemplateRepository.php
@@ -20,16 +20,15 @@ class TemplateRepository implements TemplateRepositoryInterface
 
     public function getTemplatesByCompanyId(int $companyId): ?LengthAwarePaginator
     {
-        return $this->template->select('t.id', 't.name', 't.duration', 't.timing_code', 't.created_at', 'jct.name as job_category_name', 'jsct.name as job_subcategory_name', 'l.name as language')
-            ->from('templates as t')
-            ->leftJoin('job_subcategories as jsc', 'jsc.id', 't.job_subcategory_id')
+        return $this->template->select('templates.id', 'templates.name', 'templates.duration', 'templates.timing_code', 'templates.created_at', 'jct.name as job_category_name', 'jsct.name as job_subcategory_name', 'l.name as language')
+            ->leftJoin('job_subcategories as jsc', 'jsc.id', 'templates.job_subcategory_id')
             ->leftJoin('job_subcategory_translations as jsct', 'jsct.job_subcategory_id', 'jsc.id')
             ->leftJoin('job_categories as jc', 'jc.id', 'jsc.job_category_id')
             ->leftJoin('job_category_translations as jct', 'jct.job_category_id', 'jc.id')
-            ->leftJoin('languages as l', 'l.id', 't.language_id')
-            ->where('t.company_id', $companyId)
-            ->whereColumn('jsct.language_id', 't.language_id')
-            ->whereColumn('jct.language_id', 't.language_id')
+            ->leftJoin('languages as l', 'l.id', 'templates.language_id')
+            ->where('templates.company_id', $companyId)
+            ->whereColumn('jsct.language_id', 'templates.language_id')
+            ->whereColumn('jct.language_id', 'templates.language_id')
             ->paginate();
     }
 
